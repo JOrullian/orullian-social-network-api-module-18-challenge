2cc33c80d154defb5fc613b0774b22cfd48cb9e0
JOrullian <jedediahorullian@gmail.com>
2024-10-05 13:01:56 -0500

    added thoughtSchema to Thought.js model. Also added virtual to that model to get reactionCount. Also added getter function to get human readable date format on createdAt parameter.

---
+++ models/Thought.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const formatDate = (timestamp) => {
+    return new Date(timestamp).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    });
+};
+
+// Schema to create Thought model
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: formatDate,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('reactionCount')
+    .get(function () {
+        const numberOfReactions = this.reactions.length;
+        return numberOfReactions;
+    });
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
